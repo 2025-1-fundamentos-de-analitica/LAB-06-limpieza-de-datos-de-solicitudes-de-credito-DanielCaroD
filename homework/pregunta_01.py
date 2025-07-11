8b733efd6e5f8623bf042a7013b0583c19dc23ab
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2025-07-11 12:16:36 -0500

    .

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,6 +1,47 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
+from pathlib import Path
+import pandas as pd
+
+def clean_stringcol(col: pd.Series, strip=True):
+    col = col.copy()
+    col = col.str.lower()
+    col = col.str.replace(" ", "_")
+    col = col.str.replace(".", "_")
+    col = col.str.replace("-", "_")
+    if strip:
+        col = col.str.strip()
+    return col
+
+
+def clean_df(df: pd.DataFrame):
+    df = df.copy()
+    df = df.dropna()
+    df["monto_del_credito"] = df["monto_del_credito"].str.removeprefix("$ ").str.replace(",", "")
+    df["monto_del_credito"] = df["monto_del_credito"].astype(float)
+
+    df["tipo_de_emprendimiento"] = clean_stringcol(df["tipo_de_emprendimiento"]).astype("category")
+
+    df["sexo"] = df["sexo"].str.lower().astype("category")
+
+    df["estrato"] = df["estrato"].astype("category")
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int).astype("category")
+
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True, format="mixed")
+
+    df["barrio"] = clean_stringcol(df["barrio"], strip=False).astype("category")
+
+    df["idea_negocio"] = clean_stringcol(df["idea_negocio"]).astype("category")
+
+    df["línea_credito"] = clean_stringcol(df["línea_credito"]).astype("category")
+
+    df.drop_duplicates(inplace=True)
+
+    return df
+
+from pathlib import Path
+import pandas as pd
 
 
 def pregunta_01():
@@ -13,3 +54,10 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    df = pd.read_csv("files/input/solicitudes_de_credito.csv", index_col=0, sep=";")
+    df = clean_df(df)
+    outdir = Path("files/output")
+    outdir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(outdir / "solicitudes_de_credito.csv", sep=";")
+    
+pregunta_01()
